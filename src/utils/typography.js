3678f9f0c776390377e33b70d29c6b5174af5b9a
Daniel Brady <daniel.brady@tapjoy.com>
2019-02-18 17:42:41 -0500

    Add Typography.js theme

---
+++ src/utils/typography.js
@@ -0,0 +1,7 @@
+import Typography from "typography";
+import lincoln from "typography-theme-lincoln";
+
+const typography = new Typography(lincoln);
+
+export default typography;
+export const rhythm = typography.rhythm;
