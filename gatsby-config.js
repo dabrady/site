3f97fe57ce86c3004fb1045aafd987b253572bd2
Daniel Brady <daniel.brady@tapjoy.com>
2019-02-18 17:41:22 -0500

    Update Gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,20 @@
 module.exports = {
   siteMetadata: {
-    title: "Pandas Eating Lots"
+    title: "@here",
+    description: "",
+    author: "@dabrady"
   },
 
   plugins: [
     "gatsby-transformer-remark",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
     "gatsby-plugin-emotion",
     {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "src",
-        path: `${__dirname}/src/`
+        path: `${__dirname}/src`
       }
     },
     {
@@ -22,13 +26,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: "GatsbyJS",
-        short_name: "GatsbyJS",
+        name: "a web log",
+        short_name: "Blog",
         start_url: "/",
         background_color: "#6b37bf",
         theme_color: "#6b37bf",
         display: "minimal-ui",
-        icon: "src/images/icon.png" // This path is relative to the root of the site.
+        icon: "src/images/gatsby-icon.png" // This path is relative to the root of the site.
       }
     },
     "gatsby-plugin-offline",
