faf703d532600f2d3b67b10ec0b12c084b355d08
Daniel Brady <daniel.brady@tapjoy.com>
2019-03-02 18:39:53 -0500

    Show basic post listing on home page

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -19,8 +19,7 @@ export default ({ children }) => (
     render={data => (
       <div
         css={css`
-          margin-top: ${rhythm(5)};
-          margin-left: auto;
+          margin: ${rhythm(3.4)} auto;
           max-width: 900px;
           padding: ${rhythm(2)};
           padding-top: ${rhythm(1.5)};
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,16 +1,74 @@
-import React from "react";
+/** @jsx jsx */
+import { jsx, css } from "@emotion/core";
+import { graphql, Link } from "gatsby";
+import gray from "gray-percentage";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+import { rhythm } from "../utils/typography";
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>
-      in the beginning there was darkness
-      <a href="https://www.gatsbyjs.org/">…</a>
-    </h1>
+    {data.allMarkdownRemark.edges.map(({ node }, index) => {
+      const dateWidth = rhythm(3.4);
+      return (
+        <div key={node.id}>
+          <div
+            css={css`
+              width: ${dateWidth};
+              float: left;
+              text-align: right;
+            `}
+          >
+            <h6
+              css={css`
+                margin-top: ${0};
+                margin-right: ${rhythm(2 / 3)};
+                color: ${gray(10, 0, true)};
+                font-style: italic;
+              `}
+            >
+              {node.frontmatter.date}
+            </h6>
+          </div>
+          <div
+            css={css`
+              margin-left: ${dateWidth};
+              margin-bottom: ${rhythm(1 / 4)};
+            `}
+          >
+            <Link to={node.fields.slug}>
+              <h1>{node.frontmatter.title}</h1>
+              {/* TODO Only show excerpt for "selected" post */}
+              <p>{node.excerpt}</p>
+            </Link>
+          </div>
+        </div>
+      );
+    })}
   </Layout>
 );
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: frontmatter___date, order: ASC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(fromNow: true)
+          }
+          excerpt
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default IndexPage;
