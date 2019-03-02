0eb389c6eade7e2e675c6a8744ac4ea1763896c3
Daniel Brady <daniel.brady@tapjoy.com>
2019-03-02 18:35:09 -0500

    Add initial blog post template

---
+++ src/templates/blog-post.jsx
@@ -0,0 +1,52 @@
+/** @jsx jsx */
+import { jsx, css } from "@emotion/core";
+import { graphql } from "gatsby";
+
+import { rhythm } from "../utils/typography";
+
+export default ({ data }) => {
+  const post = data.markdownRemark;
+  return (
+    <div
+      css={css`
+        margin: 0 auto;
+        max-width: 700px;
+        padding: ${rhythm(2)};
+        padding-top: ${rhythm(1.5)};
+      `}
+    >
+      <h1
+        css={css`
+          margin-bottom: ${rhythm(1 / 4)};
+        `}
+      >
+        {post.frontmatter.title}
+      </h1>
+      <h5
+        css={css`
+          color: #bbb;
+        `}
+      >
+        <em>{post.frontmatter.date}</em>
+      </h5>
+      <div
+        css={css`
+          margin-top: ${rhythm(1)};
+        `}
+        dangerouslySetInnerHTML={{ __html: post.html }}
+      />
+    </div>
+  );
+};
+
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+        date(formatString: "DD MMMM, YYYY")
+      }
+    }
+  }
+`;
