d242b1b07bdad77beb19de31ace332a9f72aa757
Daniel Brady <daniel.brady@tapjoy.com>
2020-01-22 13:54:27 +0000

    Render Signpost in default site layout

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,14 +2,12 @@ import React from "react";
 
 import Layout from "@components/layout";
 import SEO from "@components/seo";
-import SignPost from "@components/sign_post";
 
 const IndexPage = ({ data }) => {
   return (
     <Layout>
       <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
       <a href="/posts">To the blog!</a>
-      <SignPost />
     </Layout>
   );
 };
