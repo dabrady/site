2579be175dff5cefeccca89256d1b9543d319a9f
Daniel Brady <daniel.brady@tapjoy.com>
2020-01-22 13:54:27 +0000

    Render Signpost in default site layout
    [Contributes to ]

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,63 +1,34 @@
-import { Global } from "@emotion/core";
 import { Helmet } from "react-helmet";
-import { StaticQuery, graphql } from "gatsby";
-import { ThemeProvider } from "emotion-theming";
-import { space, fontSize } from "styled-system";
+import { StaticQuery, graphql, useStaticQuery } from "gatsby";
 import React from "react";
-import styled from "@emotion/styled";
 
-import { rhythm } from "@utils/typography";
+import SignPost from "@components/sign_post";
 
-const Root = styled.div`
-  display: block;
-  position: absolute;
-  height: auto;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
-  max-height: 100vh;
-  overflow: hidden;
-  max-width: 85vw;
-  margin: ${rhythm(3.4)} auto;
-  ${space}
-  ${fontSize}
-`;
-
-Root.propTypes = {
-  ...space.propTypes,
-  ...fontSize.propTypes
-};
-
-export default ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-          }
+export default ({ children }) => {
+  const {
+    site: {
+      siteMetadata: { title, siteAddr }
+    }
+  } = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+          siteAddr
         }
       }
-    `}
-    render={data => (
-      <Root px={[3, 4]}>
-        {/* TODO Due to a bug whose fix is not yet published, I need to use the
-          props method of passing children to Helmet: I run into this stack
-          overflow if I scroll through posts too fast.
-          https://github.com/nfl/react-helmet/issues/373 */}
-        <Helmet
-          meta={[{ charSet: "utf-8" }]}
-          title={data.site.siteMetadata.title}
-          link={[{ href: "https://daniel13rady.com/", rel: "canonical" }]}
-        />
-        <Global
-          styles={{
-            body: {}
-          }}
-        />
-        {children}
-      </Root>
-    )}
-  />
-);
+    }
+  `);
+  return (
+    <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>{title}</title>
+        <link href={siteAddr} rel="canonical" />
+      </Helmet>
+
+      {children}
+      {/* <SignPost>{children}</SignPost> */}
+    </div>
+  );
+};
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,13 +2,11 @@ import React from "react";
 
 import Layout from "@components/layout";
 import SEO from "@components/seo";
-import SignPost from "@components/sign_post";
 
 const IndexPage = ({ data }) => {
   return (
     <Layout>
       <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-      <SignPost />
     </Layout>
   );
 };
