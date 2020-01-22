f4e75582302fab575f0a95daff654780cb45d679
Daniel Brady <daniel.brady@tapjoy.com>
2020-01-22 13:43:53 +0000

    Determine user's nighttime in homepage component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,23 @@
 import React, { useEffect, useState } from "react";
+import SunCalc from "suncalc";
 
 import Layout from "@components/layout";
 import SEO from "@components/seo";
 
 const IndexPage = ({ data }) => {
+  // Determine if it's likely to be dark outside for the user.
+  // TODO Use this to swap themes accordingly.
+  var [nightmode, setNightmode] = useState(false);
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function getSuntimes({
+      coords: { latitude, longitude }
+    }) {
+      const now = new Date();
+      const times = SunCalc.getTimes(now, latitude, longitude);
+      setNightmode(now < times.dawn || now > times.dusk);
+    });
+  }, console.error);
+
   return (
     <Layout>
       <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
