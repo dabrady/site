f09fa0f7e5f1e0eb44f8cee71b53ab7f27ceb132
Daniel Brady <daniel.brady@tapjoy.com>
2020-10-10 16:46:51 -0400

    debugging again

--- src/lambda/stripe.js
+++ src/lambda/stripe.js
@@ -1,10 +1,13 @@
+console.debug(
+  `STRIPE_SECRET_KEY is: ${process.env.STRIPE_SECRET_KEY.substring(0, 7)}`
+);
 var stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 export async function handler(event, context) {
   var { amount } = event.queryStringParameters;
   var parsedAmount = parseFloat(amount);
   var finalAmount = parsedAmount * 100;
-  console.debug(`[server] intent to pay $${parsedAmount}`);
+  console.debug(`intent to pay $${parsedAmount}`);
   var paymentIntent = await stripe.paymentIntents.create({
     amount: finalAmount,
     currency: "usd",
