35e1767b90c20cabbf75cd8ae0a122d84f09da20
Daniel Brady <daniel.13rady@gmail.com>
2021-12-28 00:29:31 +0000

    feat: Add Nomad List icon

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -2,3 +2,4 @@ export { default as IconGithub } from "./IconGithub.jsx";
 export { default as IconDev } from "./IconDev.jsx";
 export { default as IconInstagram } from "./IconInstagram.jsx";
 export { default as IconLinkedIn } from "./IconLinkedIn.jsx";
+export { default as IconNomadList } from "./IconNomadList.jsx";
