26456ef507cb6fd95899c72b6d9d2b07c8afe859
Daniel Brady <daniel.13rady@gmail.com>
2022-08-06 23:20:21 -0700

    enhance: Change typographic scale ratio

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,8 +1,17 @@
 export const BASE_FONT_SIZE = 16; // px
 export const BASE_LINE_HEIGHT = 1.5;
 export const BASELINE = BASE_FONT_SIZE * BASE_LINE_HEIGHT;
-// A 'golden ratio'
-export const SCALE_RATIO = 1.618;
+export const Scales = {
+  GOLDEN: 1.618,
+  PERFECT_FIFTH: 1.500,
+  AUGMENTED_FOURTH: 1.414,
+  PERFECT_FOURTH: 1.333,
+  MAJOR_THIRD: 1.250,
+  MINOR_THIRD: 1.200,
+  MAJOR_SECOND: 1.125,
+  MINOR_SECOND: 1.067
+}
+export const SCALE_RATIO = Scales.AUGMENTED_FOURTH; // Looks nice with site background.
 
 // Scale of font sizes and spaces (for margin and padding) based on a common ratio.
 export const FONT_SIZES = _modularScale().map(f => `${f}em`);
