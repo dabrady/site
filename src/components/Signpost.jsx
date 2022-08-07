da9fab342f830d6e31434b0c11e52a4175990d81
Daniel Brady <daniel.13rady@gmail.com>
2022-08-06 22:50:18 -0700

    enhance(Signpost): Adjust for readability across light/dark modes

--- src/components/Signpost.jsx
+++ src/components/Signpost.jsx
@@ -4,6 +4,7 @@ import React, { useCallback, useState } from "react";
 import { graphql, useStaticQuery } from 'gatsby';
 import { getImage } from 'gatsby-plugin-image';
 import { BgImage as BackgroundImage } from 'gbimage-bridge';
+
 import useSystemTheme, { Modes } from '@utils/hooks/useSystemTheme';
 
 export default function Signpost({children}) {
@@ -12,6 +13,7 @@ export default function Signpost({children}) {
         unlitSignpost: file(relativePath: { eq: "images/full_signpost_unlit.png" }) {
           childImageSharp {
             gatsbyImageData(
+              quality: 100,
               placeholder: BLURRED,
               layout: FULL_WIDTH
             )
@@ -20,6 +22,7 @@ export default function Signpost({children}) {
         litSignpost: file(relativePath: { eq: "images/full_signpost_lit.png" }) {
           childImageSharp {
             gatsbyImageData(
+              quality: 100,
               placeholder: BLURRED,
               layout: FULL_WIDTH
             )
@@ -52,11 +55,34 @@ export default function Signpost({children}) {
       image={getImage(backgroundImage)}
       // NOTE(dabrady) This is silly, but a quirk of the `BackgroundImage` component is that
       // under the hood it applies an inline style attribute to the image tag, which takes
-      // precedence over Theme UI's `sx` styling mechanism.
-      style={{
-        backgroundPosition: 'left top'
+      // precedence over Theme UI's `sx` styling mechanism. As a result, I need to use `!important`.
+      sx={{
+        height: '100vh',
+        // unlit
+        '&:before': {
+          opacity: backgroundImage == unlitSignpost ?
+            // NOTE(dabrady) The 'unlit signpost' image has a blanket impact on readability, so I'm
+            // drastically lowering its opacity across the board.
+            '0.125 !important'
+            : '0 !important'
+        },
+        // lit
+        '&:after' : {
+          opacity: backgroundImage == unlitSignpost ?
+            '0 !important'
+            // NOTE(dabrady) At some breakpoints the text overlaps the image, harming readability.
+            // So I'm adjusting the opacity at those points as a simple remedy.
+            : [ '1 !important', '0.625 !important', '0.625 !important', '1 !important' ]
+        },
+        '&:before, &:after': {
+          // NOTE(dabrady) At some breakpoints the image is a bit to far off-screen, so I'm adjusting
+          // the position a bit to compensate.
+          backgroundPosition: [
+            'right -115px top !important',
+            'right top !important',
+          ]
+        }
       }}
-      sx={{ height: '100vh' }}
     >
       {children}
     </BackgroundImage>
