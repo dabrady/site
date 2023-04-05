1b0ab800efd85aa193af7fde0c9c8aee47740170
Daniel Brady <daniel.13rady@gmail.com>
2023-04-05 12:47:46 -1000

    feat: Update old blog post generator

--- bin/generate_blog_post.sh
+++ bin/generate_blog_post.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-cat << EOF > src/pages/posts/___.md
+cat << EOF > src/content/blog/___.md
 ---
-canonical_url:
 date: "$(gdate -Iminutes)"
 published: false
 tags:
 title: "[wip] ___"
 ---
+# What a wonderful title
+There could be anything in here. Even a boat!
 
-<!-- / -->
+{/* / */}
+
+I am an amazing excerpt of this post.
+
+You ought to hear me roar.
+
+{/* / */}
 
 EOF
