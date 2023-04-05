0e35e4b3b51e2530bd369c6f70d90aeda8e3d7db
Daniel Brady <daniel.13rady@gmail.com>
2023-04-05 11:29:55 -1000

    feat: Provide nav context to main layout tree

--- src/components/FullscreenNoScrollLayout.jsx
+++ src/components/FullscreenNoScrollLayout.jsx
@@ -1,11 +1,14 @@
 /** @jsxImportSource theme-ui */
 
 import { Global, Container } from "theme-ui";
+import { useState } from 'react';
 
 import CoreLayout from '@components/CoreLayout';
 import Terminav from "@components/Terminav";
+import { NavContext } from "@utils/hooks/useNavContext";
 
-export default function FullscreenNoScrollLayout({ children }) {
+export default function FullscreenNoScrollLayout({ children, showNavOnLoad = true }) {
+  const [showTerminav, setShowTerminav] = useState(showNavOnLoad);
   return (
     <CoreLayout>
       <Global styles={{
@@ -30,8 +33,13 @@ export default function FullscreenNoScrollLayout({ children }) {
           margin: ["2rem auto", "2.8rem auto"],
         }}
       >
-        <Terminav/>
-        {children}
+        {showTerminav && <Terminav />}
+        <NavContext.Provider value={{
+          showNav: setShowTerminav,
+          isNavShown: showTerminav
+        }}>
+          {children}
+        </NavContext.Provider>
       </Container>
     </CoreLayout>
   );
