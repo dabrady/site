b217399b67d4c9884817b5fc3e4a1a821e07d744
Daniel Brady <daniel.13rady@gmail.com>
2023-04-15 16:44:09 -1000

    style: Use 'title' layout for 'home' button of base content template

--- src/templates/BaseContent.jsx
+++ src/templates/BaseContent.jsx
@@ -1,14 +1,25 @@
 /** @jsxImportSource theme-ui */
 
+import { Heading } from "theme-ui";
+
 import BaseContentLayout from '@components/BaseContentLayout';
 import { Link } from '@components/core';
 
 export default function BaseContent({ children }) {
   return (
     <BaseContentLayout>
-      <Link href='/' sx={{ display: 'block', marginBottom: '1.3rem' }}>
-        ← Back
-      </Link>
+      <Heading as='h1' variant='text.title'>
+        <Link
+          href='/'
+          sx={{
+            display: 'block',
+            marginBottom: '1.3rem',
+            fontWeight: 'normal',
+          }}
+        >
+          ← home
+        </Link>
+      </Heading>
       {children}
     </BaseContentLayout>
   );
