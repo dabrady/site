54448ad720deb8bac5827ee4ce7cf3b0a99b0696
Daniel Brady <daniel.13rady@gmail.com>
2023-08-30 17:22:46 +0100

    style: Add timeline to blog index (#122)

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,68 +1,96 @@
 /** @jsxImportSource theme-ui */
 
-import { graphql } from "gatsby";
-import { round } from 'lodash';
+import dayjs from 'dayjs';
+import { useStaticQuery, graphql } from "gatsby";
+import { last, round } from 'lodash';
 import { Box, Heading, Paragraph } from "theme-ui";
 
 import { Link } from '@components/core';
 import theme from '@styles/theme';
 import BaseContent from '@templates/BaseContent';
 
-export default function Blog({ data }) {
-  var postNodes = data.allMdx.nodes;
-  var postCount = data.allMdx.totalCount;
+export default function Blog() {
+  var {
+    allMdx: {
+      nodes: postNodes,
+      totalCount: postCount,
+    }
+  } = useStaticQuery(graphql`
+    query {
+      allMdx(
+        filter: {
+          internal: { contentFilePath: { regex: "/src/content/blog/" } }
+
+          # NOTE(dabrady) Blog posts must be explicitly published to be listed.
+          frontmatter: { published: { eq: true } }
+        }
+        sort: { frontmatter: { date: DESC } }
+      ) {
+        totalCount
+        nodes {
+          id
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            date
+          }
+        }
+      }
+    }
+  `);
+
+  // Time things
+  var now = dayjs();
+  var newestPost = dayjs(postNodes[0].frontmatter.date);
+  var oldestPost = dayjs(last(postNodes).frontmatter.date);
+  var timeInterval = newestPost.diff(oldestPost, 'year');
+  var suffix = timeInterval > 0
+      ? `, ${timeInterval} year${timeInterval > 1 ? 's' : ''}`
+      : '';
 
   return (
     <BaseContent>
       <Box>
-        <Heading as='h1' style={{
+        <Heading as='h1' sx={{
           fontFamily: 'monospace',
           fontStyle: 'normal',
         }}>
           ./blog
         </Heading>
-        <ul>
-          {postNodes.map(({ frontmatter, fields }) => (
-            <li
-              key={fields.slug}
-              sx={theme.treelistItem}
-            >
+        <ul sx={{ paddingBottom: '1.2rem' }}>
+          {postNodes.map(({ frontmatter: { title, date }, fields }) => (
+            <li key={fields.slug} sx={theme.treelistItem}>
               <Link
                 href={fields.slug}
-                sx={{ fontFamily: 'code' }}
+                sx={{
+                  fontFamily: 'code',
+                  // NOTE(dabrady) This manually aligns wrapped titles
+                  display: 'inline-block',
+                  textIndent: [0, '-9.4rem'],
+                  paddingLeft: [0, '9.4rem'],
+                }}
               >
-                {frontmatter.title}
+                <span sx={{
+                  display: ['block', 'inline'],
+                  paddingRight: '1rem',
+                  color: 'aside',
+                  whiteSpace: 'pre-wrap',
+                }}>
+                  {/* NOTE(dabrady) Show year instead of time for posts older than the current year */}
+                  [{dayjs(date).format(`MMM DD ${now.diff(date, 'year') > 0 ? ' YYYY' : 'HH:mm'}`)}]
+                </span>
+                <span sx={{
+                }}>
+                  {title}
+                </span>
               </Link>
             </li>
           ))}
         </ul>
-        <Paragraph variant='monospace'>{postCount} posts</Paragraph>
+        <Paragraph variant='monospace'>{postCount} posts{suffix}</Paragraph>
       </Box>
     </BaseContent>
   );
 }
-export const query = graphql`
-  query {
-    allMdx(
-      filter: {
-        internal: { contentFilePath: { regex: "/src/content/blog/" } }
-
-        # NOTE(dabrady) Blog posts must be explicitly published to be listed.
-        frontmatter: { published: { eq: true } }
-      }
-      sort: { frontmatter: { date: DESC } }
-    ) {
-      totalCount
-      nodes {
-        id
-        fields {
-          slug
-        }
-        frontmatter {
-          title
-          date
-        }
-      }
-    }
-  }
-`;
