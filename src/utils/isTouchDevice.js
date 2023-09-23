be2c133a7eec08c73421ae4c588d7933fd5aa8b4
Daniel Brady <daniel.13rady@gmail.com>
2023-09-23 16:37:08 +0100

    fix NPE in server-side rendering

--- src/utils/isTouchDevice.js
+++ src/utils/isTouchDevice.js
@@ -1,4 +1,5 @@
 /** A simple but effective way of detecting touch-based devices. **/
 export default function isTouchDevice() {
-  return ('ontouchstart' in window) || navigator.maxTouchPoints > 0;
+  return (typeof window != 'undefined' && 'ontouchstart' in window)
+    || (typeof navigator != 'undefined' && navigator.maxTouchPoints > 0);
 }
